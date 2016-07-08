4a2644f05df42770f8fbb3e7731db7d787f1ed95
Christian <maritore59@yahoo.fr>
2016-07-08 11:39:14 +0200

    Changes related to Woocommerce 2.6
    $product_permalink permet maintenant de supprimer le lien vers la page
    détail

---
+++ woocommerce/cart/cart-return.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Cart return to shop
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart-totals.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+ * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+ * as little as possible, but it does happen. When this occurs the version of the template file will.
+ * be bumped and the readme will list any important changes.
+ *
+ * @see 	    http://docs.woothemes.com/document/template-structure/
+ * @author 		Christian
+ * @package 	WooCommerce/Templates
+ * @version     2.3.6
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<div class="gdb_return_to_shop">
+    <a href="<?php echo wc_get_page_permalink( 'shop' ); ?>" class="button">
+        Continuer &agrave; s&eacute;lectionner mes produits
+    </a>   
+</div>     
